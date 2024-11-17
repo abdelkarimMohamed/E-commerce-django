5c1f0a4605f48b2f833c95a59313fb6f5eee28d7
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-11-17 22:07:31 +0200

    custom users in models using AbstractBaseUSer and BaseUserManager

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+# from django.contrib.auth.models import User
+#AbstractBaseUser => to know django have usermodel maked custom
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager #responsible create  user
+import pycountry
 
-# Create your models here.
+class MyAccountManager(BaseUserManager):
+
+    def create_user(self, first_name, last_name, username,email, country, password=None):
+        if not email:
+            raise ValueError('User must have an email address.')
+        if not username:
+            raise ValueError('User must have an username.')
+    
+        user=self.model(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            country=country,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, username, first_name, last_name, password):
+
+        user=self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.is_admin=True
+        user.is_active=True
+        user.is_staff=True
+        user.is_superadmin=True
+        user.save(using=self._db)
+        return user
+    
+class Account(AbstractBaseUser):
+
+    @staticmethod
+    def get_country():
+        countries=list(pycountry.countries)
+        country_choices=[(country.alpha_2,country.name) for country in countries]
+        return country_choices
+    
+    first_name=models.CharField(max_length=250)
+    last_name=models.CharField(max_length=250)
+    email=models.EmailField(max_length=100,unique=True)
+    user_name=models.CharField(max_length=100,unique=True)
+    phone_number=models.CharField(max_length=50)
+    country=models.CharField(max_length=2,choices=get_country(),default='US')
+
+    date_joined=models.DateTimeField(auto_now_add=True)
+    last_login=models.DateTimeField(auto_now_add=True)
+
+    is_active=models.BooleanField(default=False)
+    is_staff=models.BooleanField(default=False)
+    is_superadmin=models.BooleanField(default=False)
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['first_name','last_name']
+
+    objects=MyAccountManager
+
+    def __str__(self):
+        return self.email
+    
+    def her_perm(self,perm,obj=None):  #check user if admin show dashboard inverse not show dashboard
+        return self.is_admin
+    
+    def has_module_perms(self,app_label): #show all apps to any admin in dashboard
+        return True
