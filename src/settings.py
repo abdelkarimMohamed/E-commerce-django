67c478146e695187115ece1dce05ac66afde9114
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-11-25 09:46:10 +0200

    add postgresql

--- src/settings.py
+++ src/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stores',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
