4b0fe2dbae014139304ce01171d81b992983b3ef
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-11-28 15:34:01 +0200

    add searchvector && searchQuery && SearchRank in search products

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,5 @@ urlpatterns=[
     path('',views.list_product,name='list_of_products'),
     path('category/<slug:category_slug>/',views.list_product,name='products_by_category'),
     path('product/<slug:product_slug>/',views.product_detail,name='product_detail'),
+    path('search/',views.product_search,name='product_search'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Category,Product
-
+from django.contrib.postgres.search import SearchVector,SearchRank,SearchQuery
 
 
 def list_product(request,category_slug=None):
@@ -30,3 +30,22 @@ def product_detail(request,product_slug):
         'detail':product
     }
     return render(request,'store/product_detail.html',context)
+
+def product_search(request):
+
+    query=None
+    results=[]
+    if 'query' in request.GET:
+        query=request.GET.get('query')
+        search_vector=SearchVector('name','description')
+        search_query=SearchQuery(query) # get to similar words
+        results=Product.objects.annotate(search=search_vector,rank=SearchRank(search_vector,search_query)).filter(search=search_query,status=Product.Status.AVAILABLE).order_by('-rank')
+        # results=Product.objects.annotate(search=SearchVector('name','description'),rank=SearchRank(SearchVector('name','description'),SearchQuery(query))).filter(search=SearchQuery(query),status=Product.Status.AVAILABLE).order_by('-rank') 
+        #create new field name search inside in field search results search from name,desc  Temporarily in DB 
+        
+    context={
+        'query':query,
+        'results':results
+    }
+
+    return render(request,'store/search.html',context)
