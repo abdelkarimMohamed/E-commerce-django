e2cdbe6651b076535b9f91a9a6100b022e8faa2c
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-12-04 19:11:19 +0200

    add function add and remove and cartdetail in views and create form for quantity and override quantity and add cusom urls

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.views.decorators.http import require_POST
+from store.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
 
-# Create your views here.
+
+@require_POST
+def cart_add(request,product_id):
+
+    cart=Cart(request)
+    product=get_object_or_404(Product,id=product_id,status=Product.Status.AVAILABLE)
+    form=CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd=form.cleaned_data
+        quantity=cd['quantity']
+        cart.add(product,quantity,override_quantity=cd['override'])
+        return redirect('cart:cart_detail')
+
+
+@require_POST
+def cart_remove(request,product_id):
+    cart=Cart(request)
+    product=get_object_or_404(Product,id=product_id,status=Product.Status.AVAILABLE)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    cart=Cart(request)
+    context={'cart':cart}
+    return render(request,'cart/cart_details.html',context)
