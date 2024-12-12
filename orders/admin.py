cb6461b730c024dea886353728d3f0698357aa49
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-12-12 16:12:05 +0200

    add from order and create model OrderItem and create in dashborad admin orderitem under order and create function in orders Order_create to create orderitem using order form and using cart to add in orderitem products and price and quantity

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order,OrderItem
+
+
+class OrderItemInline(admin.TabularInline):   #TabularInline => OrderAdminيظهر تحت جدول ال 
+    model=OrderItem
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display=['first_name','email','paid','created_at']
+    inlines=[OrderItemInline]
 
--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import random
 import string
 from django.utils import timezone
+from store.models import Product
 
 def generate_order_id(length=8):
     characters=string.ascii_letters + string.digits
@@ -38,3 +39,16 @@ class Order(models.Model):
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+    
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order,related_name='items',on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,related_name='order_item',on_delete=models.CASCADE)
+    price=models.DecimalField(max_digits=6,decimal_places=2)
+    quantity=models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+    
+    def get_cost(self):
+
+        return self.price * self.quantity
